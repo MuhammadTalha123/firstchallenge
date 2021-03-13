7c3fd9c38407ca40c6110b5a7779e1d6a40700e9
MuhammadTalha123 <59221584+MuhammadTalha123@users.noreply.github.com>
2021-03-13 14:24:23 +0500

    update color and css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./App.css";
-import PrimarySearchAppBar from "./NavBar";
 import MediaCard from "./Cards";
-import JSONDATA from "./MOCK-JSON-DATA.json";
 import Cart from "./Cart";
 import history from "./history";
 import { Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  console.log(JSONDATA);
   return (
     <div className="App">
       <Router history={history}>
